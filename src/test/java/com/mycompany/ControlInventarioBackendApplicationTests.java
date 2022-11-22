f3ae9e07c64e611b27c4683920b07b1cdbf2f216
jeanct <jeancocht@gmail.com>
2022-11-22 10:59:37 -0500

    proyecto base

---
+++ src/test/java/com/mycompany/ControlInventarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlInventarioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
